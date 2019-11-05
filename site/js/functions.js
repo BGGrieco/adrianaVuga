ee7ec5dd844082b8f8e095c25899dcef2bd26cad
BGGrieco <brian.g.grieco@gmail.com>
2019-11-05 09:27:55 +0000

    Reformatted js code

--- site/js/functions.js
+++ site/js/functions.js
@@ -1,27 +1,22 @@
-function openNav() 
-{
+function openNav() {
 	"use strict";
-    document.getElementById("mySidenav").style.width = "200px";
+	document.getElementById("mySidenav").style.width = "200px";
 }
 
-function closeNav() 
-{
+function closeNav() {
 	"use strict";
-    document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("mySidenav").style.width = "0";
 }
 
-function locatorMapOne() 
-{
+function locatorMapOne() {
 	"use strict";
     var studioOne = {lat: 45.542703, lng: 9.078482000000008};
-    var map = new google.maps.Map(document.getElementById("mapOne"), 
-	{
-        zoom: 15,
-        center: studioOne
+    var map = new google.maps.Map(document.getElementById("mapOne"), {
+      zoom: 15,
+      center: studioOne
     });
-    var marker = new google.maps.Marker
-	({
-        position: studioOne,
-        map: map
-    });
+    var marker = new google.maps.Marker({
+    position: studioOne,
+    map: map
+	});
 }
