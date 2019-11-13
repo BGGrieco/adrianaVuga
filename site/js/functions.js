ab1f03c5929b94b3529d5d86ab72b1b3a3980130
Brian Grieco <bgrieco@L-MacBook-1552.local>
2019-11-13 17:16:11 +0000

    Formatted code

--- site/js/functions.js
+++ site/js/functions.js
@@ -1,22 +1,25 @@
 function openNav() {
-	"use strict";
-	document.getElementById("mySidenav").style.width = "200px";
+  "use strict";
+  document.getElementById("mySidenav").style.width = "200px";
 }
 
 function closeNav() {
-	"use strict";
-	document.getElementById("mySidenav").style.width = "0";
+  "use strict";
+  document.getElementById("mySidenav").style.width = "0";
 }
 
 function locatorMapOne() {
-	"use strict";
-    var studioOne = {lat: 45.542703, lng: 9.078482000000008};
-    var map = new google.maps.Map(document.getElementById("mapOne"), {
-      zoom: 15,
-      center: studioOne
-    });
-    var marker = new google.maps.Marker({
+  "use strict";
+  var studioOne = {
+    lat: 45.542703,
+    lng: 9.078482000000008
+  };
+  var map = new google.maps.Map(document.getElementById("mapOne"), {
+    zoom: 15,
+    center: studioOne
+  });
+  var marker = new google.maps.Marker({
     position: studioOne,
     map: map
-	});
+  });
 }
